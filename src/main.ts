39aee14341ade3c24e4b703a5289dae717b71e89
Daan Verbeek <daanverbeek13@gmail.com>
2025-02-12 15:00:04 +0100

    Added swagger & prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Code Dual API')
+    .setDescription('The Code Dual API description')
+    .setVersion('1.0')
+    .addTag('code-dual')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
