c24a076f60ff33b9175a8e75641fd569908bef1e
Daan Verbeek <daanverbeek13@gmail.com>
2025-03-28 23:40:13 +0100

    commented stripe implentation out

--- src/modules/subscription/subscription.service.ts
+++ src/modules/subscription/subscription.service.ts
@@ -6,13 +6,13 @@ import Stripe from 'stripe';
 export class SubscriptionService {
   private readonly stripe: Stripe;
   constructor(private readonly prisma: PrismaService) {
-    if (!process.env.STRIPE_SECRET_KEY || !process.env.STRIPE_ACCOUNT_ID) {
-      throw new Error('STRIPE_SECRET_KEY or STRIPE_ACCOUNT_ID is not set');
-    }
-    this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: '2025-01-27.acacia',
-      stripeAccount: process.env.STRIPE_ACCOUNT_ID,
-    });
+    // if (!process.env.STRIPE_SECRET_KEY || !process.env.STRIPE_ACCOUNT_ID) {
+    //   throw new Error('STRIPE_SECRET_KEY or STRIPE_ACCOUNT_ID is not set');
+    // }
+    // this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    //   apiVersion: '2025-01-27.acacia',
+    //   stripeAccount: process.env.STRIPE_ACCOUNT_ID,
+    // });
   }
 
   async getCurrentSubscription(userId: string) {
